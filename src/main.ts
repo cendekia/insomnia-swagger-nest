d2ac84b1994f53978193e91f11c7820d3e5aad9c
cendekia <cendekia@sonar.id>
2023-02-19 21:28:34 +0700

    step 2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Insonmia Swagger NestJS')
+    .setDescription('Insonmia Swagger NestJS description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000')
+    .addTag('Users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty({
+    type: String,
+    default: 'John Doe',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    type: String,
+    default: 'john.doe@test.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    type: String,
+    default: 'Qwerty!23',
+  })
   @IsNotEmpty()
   @MaxLength(255)
   @MinLength(8)
