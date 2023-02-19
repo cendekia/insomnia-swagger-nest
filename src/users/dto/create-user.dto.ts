ea1da737c9a649c0c381873680ff9f6f94dc5889
cendekia <cendekia@sonar.id>
2023-02-19 20:19:02 +0700

    step-1

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MaxLength(255)
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+}
